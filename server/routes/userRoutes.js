100296bb307ac0003870795c9fa731851cc34300
Manyathaaa <mailtomanyatha@gmail.com>
2025-08-07 14:21:36 +0530

    added updateUser in userRoute

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,9 +1,14 @@
 import express from "express";
-import { getuserController } from "../controllers/userController.js";
+import {
+  getuserController,
+  UpdateUserController,
+} from "../controllers/userController.js";
 import { requireSignIn } from "../middleware/authmiddleware.js";
 
 const router = express.Router();
 
 router.get("/get-user", requireSignIn, getuserController);
+
+router.patch("/update-user", requireSignIn, UpdateUserController);
 
 export default router;
