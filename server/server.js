296122de1f5ebaf716eeef29d811e49aeedf4493
Manyathaaa <mailtomanyatha@gmail.com>
2025-08-07 14:40:16 +0530

    fix : fixed route endpoint

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ app.use(express.json());
 //routes
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/user", userRoutes);
-app.use("/api/posts", postRoutes);
+app.use("/api/v1/posts", postRoutes);
 
 const PORT = process.env.PORT || 1011;
 
