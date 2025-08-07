98d6d96986093546f6de8788ff6735c0d4c0819d
Manyathaaa <mailtomanyatha@gmail.com>
2025-08-07 14:22:24 +0530

    postRoutes created and added

---
+++ server/routes/postRoutes.js
@@ -0,0 +1,17 @@
+// server/routes/postRoutes.js
+import express from "express";
+import {
+  createPostController,
+  getAllPostsController,
+} from "../controllers/postController.js";
+import { requireSignIn } from "../middleware/authmiddleware.js";
+
+const router = express.Router();
+
+// Protected route to create post
+router.post("/create", requireSignIn, createPostController);
+
+// Public route to get all posts
+router.get("/all", getAllPostsController);
+
+export default router;
--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ import cors from "cors";
 import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
+import postRoutes from "./routes/postRoutes.js";
 
 dotenv.config();
 connectDB();
@@ -15,6 +16,7 @@ app.use(express.json());
 //routes
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/user", userRoutes);
+app.use("/api/posts", postRoutes);
 
 const PORT = process.env.PORT || 1011;
 
