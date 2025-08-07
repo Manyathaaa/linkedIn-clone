d6aa1691f82be24694fada648883fec10880ed17
Manyathaaa <mailtomanyatha@gmail.com>
2025-08-07 18:42:38 +0530

    Installed morgan and cors setup to frontend

--- server/server.js
+++ server/server.js
@@ -5,13 +5,21 @@ import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import postRoutes from "./routes/postRoutes.js";
+import morgan from "morgan";
 
 dotenv.config();
 connectDB();
 
 const app = express();
-app.use(cors());
+
 app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
+app.use(morgan("dev"));
 
 //routes
 app.use("/api/v1/auth", authRoutes);
